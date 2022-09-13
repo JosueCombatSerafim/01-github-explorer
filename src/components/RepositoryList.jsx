5d611f1f5165c25c337653203e4edf5d34e77c10
Josué Combat Serafim <jcombatserafim@gmail.com>
2022-09-13 20:55:26 -0300

    feat: start a repository list block

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const repositoryName = "unform2";
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Formis in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Formis in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Formis in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Formis in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
